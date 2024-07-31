7264410851150eebe8eb410932107009a5b2617b
Omar Estrada <omarestrada908@gmail.com>
2024-07-30 23:28:11 -0500

    initial commit

---
+++ app/src/main/java/com/puchunguita/cbzconverter/MainActivity.kt
@@ -0,0 +1,128 @@
+package com.puchunguita.cbzconverter
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.provider.OpenableColumns
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.puchunguita.cbzconverter.ui.theme.CbzConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CbzConverterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(
+                        viewModel = MainViewModel(application),
+                        modifier = Modifier.padding(innerPadding)
+                    )
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+                }
+            }
+        }
+    }
+}
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    CbzConverterTheme {
+//        Greeting("Android")
+//    }
+//}
+
+@Composable
+fun MainScreen(viewModel: MainViewModel, modifier: Modifier = Modifier) {
+    var selectedFileUri by remember { mutableStateOf<Uri?>(null) }
+    var fileName by remember { mutableStateOf("No file selected") }
+    val context = LocalContext.current
+
+    val filePickerLauncher = rememberLauncherForActivityResult(ActivityResultContracts.OpenDocument()) { uri: Uri? ->
+        uri?.let {
+            selectedFileUri = it
+            fileName = it.getFileName(context)
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = fileName, modifier = Modifier.padding(bottom = 16.dp))
+
+        Button(onClick = { filePickerLauncher.launch(arrayOf("*/*")) }) {
+            Text(text = "Select CBZ File")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { selectedFileUri?.let { viewModel.convertToEPUB(it) } },
+            enabled = selectedFileUri != null
+        ) {
+            Text(text = "Convert to EPUB")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { selectedFileUri?.let { viewModel.convertToPDF(it) } },
+            enabled = selectedFileUri != null
+        ) {
+            Text(text = "Convert to PDF")
+        }
+    }
+}
+
+fun Uri.getFileName(context: Context): String {
+    var name = "Unknown"
+    context.contentResolver.query(
+        this,
+        null,
+        null,
+        null,
+        null
+    )?.use { cursor ->
+        val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+        cursor.moveToFirst()
+        name = cursor.getString(nameIndex)
+    }
+    return name
+}
---
+++ app/src/main/java/com/puchunguita/cbzconverter/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.puchunguita.cbzconverter
+
+import android.app.Application
+import android.net.Uri
+import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val context = getApplication<Application>().applicationContext
+
+    fun convertToEPUB(fileUri: Uri) {
+        CoroutineScope(Dispatchers.IO).launch {
+            // Add your conversion logic here
+            withContext(Dispatchers.Main) {
+                // Update UI after conversion if needed
+            }
+        }
+    }
+
+    fun convertToPDF(fileUri: Uri) {
+        CoroutineScope(Dispatchers.IO).launch {
+            // Add your conversion logic here
+            withContext(Dispatchers.Main) {
+                // Update UI after conversion if needed
+                val bitmaps = extractImagesFromCBZ(fileUri, context)
+                val pdfFile = convertToPDF(bitmaps, context)
+                // Handle the PDF file as needed (e.g., show a notification, open it, etc.)
+
+            }
+        }
+    }
+}
