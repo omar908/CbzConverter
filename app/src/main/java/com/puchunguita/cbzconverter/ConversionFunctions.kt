0cd5d86d123e9fe15bb0e4061afbd9eea03ad3c6
Omar Estrada <omarestrada908@gmail.com>
2024-08-04 21:11:18 -0500

    Refactored ConversionFunctions.kt for better readability, and removed unneeded variables into new functions. Also closing inputStream after copying to cache, and deleting cache files once PDF File(s) is created.

--- app/src/main/java/com/puchunguita/cbzconverter/ConversionFunctions.kt
+++ app/src/main/java/com/puchunguita/cbzconverter/ConversionFunctions.kt
@@ -10,6 +10,7 @@ import com.itextpdf.layout.Document
 import com.itextpdf.layout.element.Image
 import java.io.File
 import java.io.InputStream
+import java.util.Enumeration
 import java.util.logging.Logger
 import java.util.stream.IntStream
 import java.util.zip.ZipEntry
@@ -25,81 +26,125 @@ fun convertCbzToPDF(
     outputFileName: String = "output.pdf"
 ): List<File> {
     val inputStream = context.contentResolver.openInputStream(fileUri) ?: return mutableListOf<File>()
+    val tempFile = copyCbzToCache(context, inputStream)
 
-    // Copy the CBZ file to a temporary location
+    // Open the CBZ file as a zip
+    val zipFile = ZipFile(tempFile)
+    val zipFileEntries = zipFile.entries()
+    val totalNumberOfImages = zipFile.size()
+
+    val outputFiles = mutableListOf<File>()
+    val downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+
+    if (!downloadsFolder.exists()) { downloadsFolder.mkdirs() }
+
+    if (totalNumberOfImages > maxNumberOfPages) {
+        createMultiplePdfFromCbz(
+            totalNumberOfImages,
+            maxNumberOfPages,
+            zipFileEntries,
+            outputFileName,
+            downloadsFolder,
+            subStepStatusAction,
+            zipFile,
+            outputFiles
+        )
+    } else {
+        createSinglePdfFromCbz(
+            zipFileEntries,
+            outputFileName,
+            downloadsFolder,
+            subStepStatusAction,
+            totalNumberOfImages,
+            zipFile,
+            outputFiles
+        )
+    }
+
+    zipFile.close()
+    tempFile.delete()
+    return outputFiles
+}
+
+private fun copyCbzToCache(context: Context, inputStream: InputStream): File {
     val tempFile = File(context.cacheDir, "temp.cbz")
     tempFile.outputStream().use { outputStream ->
         inputStream.copyTo(outputStream)
     }
+    inputStream.close()
+    return tempFile
+}
 
-    // Open the CBZ file as a zip
-    val zipFile = ZipFile(tempFile)
-    val totalNumberOfImages = zipFile.size()
-    val zipFileEntries = zipFile.entries()
+private fun createMultiplePdfFromCbz(
+    totalNumberOfImages: Int,
+    maxNumberOfPages: Int,
+    zipFileEntries: Enumeration<out ZipEntry>,
+    outputFileName: String,
+    downloadsFolder: File?,
+    subStepStatusAction: (String) -> Unit,
+    zipFile: ZipFile,
+    outputFiles: MutableList<File>
+) {
+    val amountOfFilesToExport = ceil(totalNumberOfImages.toDouble() / maxNumberOfPages).toInt()
+    val zipFileEntriesList = zipFileEntries.toList()
 
-    val downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-    if (!downloadsFolder.exists()) {
-        downloadsFolder.mkdirs()
-    }
-    val outputFiles = mutableListOf<File>()
-    var amountOfFilesToExport = 1
-    var outputFile = File(downloadsFolder, outputFileName)
+    IntStream.range(0, amountOfFilesToExport).forEach { index ->
+        val newOutputFileName = outputFileName.replace(".pdf", "_part-${index + 1}.pdf")
+        val outputFile = File(downloadsFolder, newOutputFileName)
+        val startIndex = index.times(maxNumberOfPages)
+        val nextPossibleEndIndex = index.plus(1).times(maxNumberOfPages)
+        val endIndex =
+            if (nextPossibleEndIndex > totalNumberOfImages) totalNumberOfImages else nextPossibleEndIndex
+        val imagesToProcess = zipFileEntriesList.subList(startIndex, endIndex)
 
-    if (totalNumberOfImages > maxNumberOfPages) {
-        amountOfFilesToExport = ceil(totalNumberOfImages.toDouble() / maxNumberOfPages).toInt()
-        val zipFileEntriesList = zipFileEntries.toList()
-
-        IntStream.range(0, amountOfFilesToExport).forEach { index ->
-            val newOutputFileName = outputFileName.replace(".pdf", "_part-${index + 1}.pdf")
-            outputFile = File(downloadsFolder, newOutputFileName)
-            val startIndex = index.times(maxNumberOfPages)
-            val nextPossibleEndIndex = index.plus(1).times(maxNumberOfPages)
-            val endIndex = if (nextPossibleEndIndex > totalNumberOfImages) totalNumberOfImages else nextPossibleEndIndex
-            val imagesToProcess = zipFileEntriesList.subList(startIndex, endIndex)
-
-            PdfWriter(outputFile.absolutePath).use { writer ->
-                PdfDocument(writer).use { pdfDoc ->
-                    Document(pdfDoc).use { document ->
-                        for ((currentImageIndex, imageFile) in imagesToProcess.withIndex()) {
-                            subStepStatusAction(
-                                "Processing part ${index + 1} of $amountOfFilesToExport " +
-                                        "- Processing image file " +
-                                        "${index.times(maxNumberOfPages) + currentImageIndex + 1} " +
-                                        "of $totalNumberOfImages"
-                            )
-                            extractImageAndAddToPDFDocument(zipFile, imageFile, document)
-                        }
-                    }
-                }
-                outputFiles.add(outputFile)
-            }
-
-
-        }
-    } else {
-        // Create the PdfWriter and PdfDocument
         PdfWriter(outputFile.absolutePath).use { writer ->
             PdfDocument(writer).use { pdfDoc ->
                 Document(pdfDoc).use { document ->
-                    // Counter used for logging the progress only
-                    var counter = 1
-                    while (zipFileEntries.hasMoreElements()) {
+                    for ((currentImageIndex, imageFile) in imagesToProcess.withIndex()) {
                         subStepStatusAction(
-                            "Processing image file " +
-                                    "$counter " +
+                            "Processing part ${index + 1} of $amountOfFilesToExport " +
+                                    "- Processing image file " +
+                                    "${index.times(maxNumberOfPages) + currentImageIndex + 1} " +
                                     "of $totalNumberOfImages"
                         )
-                        extractImageAndAddToPDFDocument(zipFile, zipFileEntries.nextElement(), document)
-                        counter++
+                        extractImageAndAddToPDFDocument(zipFile, imageFile, document)
                     }
                 }
             }
             outputFiles.add(outputFile)
         }
     }
+}
 
-    zipFile.close()
-    return outputFiles
+private fun createSinglePdfFromCbz(
+    zipFileEntries: Enumeration<out ZipEntry>,
+    outputFileName: String,
+    downloadsFolder: File?,
+    subStepStatusAction: (String) -> Unit,
+    totalNumberOfImages: Int,
+    zipFile: ZipFile,
+    outputFiles: MutableList<File>
+) {
+    val outputFile = File(downloadsFolder, outputFileName)
+
+    PdfWriter(outputFile.absolutePath).use { writer ->
+        PdfDocument(writer).use { pdfDoc ->
+            Document(pdfDoc).use { document ->
+                // Counter used for logging the progress only
+                var counter = 1
+                while (zipFileEntries.hasMoreElements()) {
+                    subStepStatusAction(
+                        "Processing image file " +
+                                "$counter " +
+                                "of $totalNumberOfImages"
+                    )
+                    extractImageAndAddToPDFDocument(zipFile, zipFileEntries.nextElement(), document)
+                    counter++
+                }
+            }
+        }
+        outputFiles.add(outputFile)
+    }
 }
 
 private fun extractImageAndAddToPDFDocument(
